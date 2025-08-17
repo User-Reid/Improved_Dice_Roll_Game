a5b2f61122f685fd3834b399dda6b21e8c95e3dc
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-17 09:45:03 -0500

    beginning improved diceroll game

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿var random = new Random();
+int diceRoll = random.Next(1, 7);
+
+Console.ReadLine();
+
+class Dice
+{
+  private readonly Random _random;
+
+  public Dice(Random random)
+  {
+    _random = random;
+  }
+
+  public int Roll()
+  {
+    return _random.Next(1, 7);
+  }
+}
